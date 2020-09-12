e9832db66c72ee8835a375b6065944d335f55003
Elena Yanchenko <veleno.ajt@gmail.com>
2020-09-12 15:36:37 +0300

    add soltion carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || /[A-Za-z]/.test(sampleActivity) || +sampleActivity >= MODERN_ACTIVITY || +sampleActivity <= 0) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k);
 };
