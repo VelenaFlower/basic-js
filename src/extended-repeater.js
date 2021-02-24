d2505d6cf1632d89b7fcbd7f9d2559571129c03c
Elena Yanchenko <veleno.ajt@gmail.com>
2021-02-24 22:32:05 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = String(arguments[0]);
+  options = arguments[1];
+
+  if (options.addition !== undefined || options.addition === null) {
+      options.addition = String(options.addition);
+  }
+  if (!options.repeatTimes) {
+      options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes === undefined) {
+      options.additionRepeatTimes = 1;
+  }
+  if (options.separator === undefined) {
+      options.separator = '+';
+  }
+  if (options.additionSeparator === undefined) {
+      options.additionSeparator = '|';
+  }
+
+  let addArr = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+
+  return new Array(options.repeatTimes).fill(str + addArr).join(options.separator);
 };
   
