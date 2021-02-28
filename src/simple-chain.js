f3952d6d360d358ce3d88706565f3931fb22032f
Elena Yanchenko <veleno.ajt@gmail.com>
2021-02-28 22:08:33 +0300

    feat: add solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,34 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
+  chain: [],
+
+   getLength() {
+    return this.chain.length;
+   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push(' ');
+    }
+    else {
+      this.chain.push('( ' + String(value) + ' )')
+    }
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
+    if (typeof(position) !== "number" || position < 1 || position > this.chain.length) {
+      this.chain = [];
+      throw new Error;
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
+   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+   this.chain.reverse();
+   return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
